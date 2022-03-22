a975334b7057b5dd1690e2698efcea28060a4168
Melany <mraediaz@gmail.com>
2022-03-22 14:09:05 +0100

    refractored java, no longer hard coded

--- script.js
+++ script.js
@@ -1,41 +1,89 @@
+// information stored in a map of arrays. 
+// participants = ["name", [income, percentage of expense, expense contribution, percentage of income]]
+function getParticipants() {
+    var expenseValue = document.getElementById("expenseValue").value;
+
+    //get input information
+    var x = document.getElementsByName("inputParticipant");
+    var participants = new Map();
+    for (var i of x.values()) {
+        participants.set(i.children['name']['firstElementChild'].value, [i.children['income']['firstElementChild'].value]);
+    }
+
+
+    //calculations
+    let totalIncome = 0;
+    participants.forEach(function(value, key) {
+        //remove commas
+        value[0] = value[0].toString().replace(/\,/g, '');
+        value[0] = Number(value[0]);
+
+        //check for negative numbers and words
+        if (value[0] < 0) {
+            value[0] = 0;
+        }
+        if (isNaN(value[0])) {
+            alert("Only numbers please");
+        }
+
+        //get summation of incomes
+        totalIncome += value[0];
+    });
+
+
+    participants.forEach(function(value, key) {
+        //percentage of expense
+        value[1] = value[0] / totalIncome;
+
+        //expense contribution
+        value[2] = (value[1] * parseFloat(expenseValue)).toFixed(2);
+        value[1] = (value[1] * 100).toFixed(2);
+
+        //percentage of income
+        value[3] = (value[2] / value[0])
+        value[3] = (value[3] * 100).toFixed(2);
+
+        //catch errors
+        for (const j in value) {
+            if (!value[j] || !isFinite(value[j])) { // Matches +0, -0, NaN
+                value[j] = 0;
+            }
+        }
+
+
+    });
+    // console.log(...participants);
+    return participants;
+}
+
+
+
+
 function myFunction() {
- 
-  var expenseValue = document.getElementById("expense-value");
-  
-  var participantA = document.getElementById("participant-A");
-  var participantAIncome = document.getElementById("participant-A-income");
-  var participantB = document.getElementById("participant-B");
-  var participantBIncome = document.getElementById("participant-B-income");
+    var textOut = "";
+    var participants = getParticipants();
 
-  var totalIncome = parseFloat(participantAIncome.value) + parseFloat(participantBIncome.value);
-  
-  var percentageA = parseFloat(participantAIncome.value/totalIncome);
-  var rentA = percentageA*expenseValue.value;
-  
-  var percentageB = parseFloat(participantBIncome.value/totalIncome);
-  var rentB = percentageB*expenseValue.value;
-  
-  let buttons = Array.from(document.getElementById("results").getElementsByClassName('cell'));
-  // console.log(buttons);
-  buttons[0].innerText = participantA.value;
-  buttons[1].innerText = participantB.value;
-  buttons[2].innerText = "$" + rentA.toFixed(2);
-  buttons[3].innerText = "$" + rentB.toFixed(2);
-  buttons[4].innerText = (percentageA*100).toFixed(2) + "%";
-  buttons[5].innerText = (percentageB*100).toFixed(2) + "%";
-  buttons[6].innerText = ((rentA/participantAIncome.value)*100).toFixed(2) + "%";
-  buttons[7].innerText = ((rentB/participantBIncome.value)*100).toFixed(2) + "%";
+    let index = 0;
+    participants.forEach(function(value, key) {
+        console.log(key + value);
 
-// test
-// buttons.map( button => {
-      // console.log(buttons);
-      // for (let i = 4; i < buttons.length; i++) {
-      //   buttons[i].innerText = "test";
-      //   console.log(buttons[i].getAttribute("id"));
-//end test
+        let buttons = Array.from(document.getElementById("results").getElementsByClassName('cell'));
+        // for(let i = 0; i < buttons.length; i++){
+        buttons[index + 0].innerText = key;
+        buttons[index + 2].innerText = "$" + value[2];
+        buttons[index + 4].innerText = value[1] + "%";
+        buttons[index + 6].innerText = value[3] + "%";
+        // }
 
+        console.log(buttons);
 
-document.getElementById("text-results-A").innerText = participantA.value + " owes: $" + rentA.toFixed(2) + " towards the expense.\nThis is " + (percentageA*100).toFixed(2) + "% of the expense and " + ((rentA/participantAIncome.value)*100).toFixed(2) + "% of their income/budget.";
-document.getElementById("text-results-B").innerText = participantB.value + " owes: $" + rentB.toFixed(2) + " towards the expense.\nThis is " + (percentageB*100).toFixed(2) + "% of the expense and " + ((rentB/participantBIncome.value)*100).toFixed(2) + "% of their income/budget.";
+        textOut += key + " owes: $" +
+            value[2] + " towards the expense.\nThis is " +
+            value[1] + "% of the expense and " +
+            value[3] + "% of their income/budget.\n\n";
+
+        index++;
+    }); //end foreach loop
+    document.getElementById("textResults").innerText = textOut;
 
 }
