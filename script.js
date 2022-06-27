4b8ca315a4bb741eb9bbea63ec8fa6445cf67b60
Melany <mraediaz@gmail.com>
2022-06-27 12:51:24 +0200

    added collapsable

--- script.js
+++ script.js
@@ -153,3 +153,19 @@ function myFunction() {
     document.getElementById("textResults").innerText = textOut;
 
 }
+
+// Collapsable information
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
